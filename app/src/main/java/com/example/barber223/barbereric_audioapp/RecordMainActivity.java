346787b0e7d17e2fb47fc2045c217311ad433e8d
Eric Barber <eabarber@fullsail.edu>
2018-05-11 18:32:02 -0400

    Fixed the bug with requesting permissions:)

--- app/src/main/java/com/example/barber223/barbereric_audioapp/RecordMainActivity.java
+++ app/src/main/java/com/example/barber223/barbereric_audioapp/RecordMainActivity.java
@@ -5,6 +5,7 @@
 package com.example.barber223.barbereric_audioapp;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -21,19 +22,12 @@ import com.example.barber223.barbereric_audioapp.Interfaces.SelectionFragmentInt
 
 public class RecordMainActivity extends AppCompatActivity implements SelectionFragmentInterface{
 
-    private String[] mPermissions = {Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
-    Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
 
                 //need to inflate the fragments for the views
         getFragmentManager().beginTransaction()
@@ -42,6 +36,12 @@ public class RecordMainActivity extends AppCompatActivity implements SelectionFr
                 .replace(R.id.information_fragment_frame, RecordInformationFragment.newInstance())
                 .commit();
         //have to decide if the files are going to be held within a service or an activity
+        String[] mPermissions = {Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO};
+
+        if (!hasPermisisons(this, mPermissions)){
+            ActivityCompat.requestPermissions(this, mPermissions, 0x0101);
+        }
 
         /*
         for (int i = 0; i < mPermissions.length; i ++){
@@ -54,6 +54,17 @@ public class RecordMainActivity extends AppCompatActivity implements SelectionFr
             }
         }
         */
+    }
+
+    private boolean hasPermisisons(Context _context, String... permissions){
+        if (_context != null && permissions != null){
+            for (String permission: permissions){
+                if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
